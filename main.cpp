4932116816880e6963a517915f3ceb48cb5c17f8
josselin <josselin@debian.com>
2019-11-08 20:41:33 +0100

    makefile

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <limits>
 #include <queue>
 #include "utils.h"
-#include "image_through.h"
 #include "image2d.h"
 #include "box2d_iterator.h"
 #include "partial_box2d_iterator.h"
@@ -10,7 +9,7 @@
 #include "partial_image2d.h"
 #include "image_through.h"
 
-using bool_t = unsigned;
+using bool_t = unsigned ;
 
 template <typename I>
 image2d<unsigned> compute_dmap(const I& input)
@@ -50,7 +49,6 @@ image2d<unsigned> compute_dmap(const I& input)
         q.pop();
         n.center_at(p);
         for (n.start(); n.is_valid(); n.next())
-
             if (D.has(n.value()) and dmap(n.value()) == max)
             {
                 dmap(n.value()) = dmap(p) + 1;
@@ -60,8 +58,9 @@ image2d<unsigned> compute_dmap(const I& input)
     return dmap;
 }
 
-int main() {
 
+
+int main() {
 
     std::vector<unsigned> v{2, 1, 1, 1, 0,
                             0, 0, 0, 1, 0,
@@ -70,30 +69,19 @@ int main() {
                             1, 1, 0, 0, 0,
                             0, 1, 1, 1, 3};
 
-
-    image2d<bool_t> a = image2d<bool_t>(4, 4, v);
-
-    std::cout << "Image de base" << std::endl;
-    utils::print_image(std::cout, a);
-    auto d = compute_dmap(a);
-    std::cout << "Image modifiée" << std::endl;
-    utils::print_image(std::cout, d);
-
-    //box2d D{2, 3};
-    // TODO : ask for the meaning of the line above
-    image2d<unsigned> ima(5,5);
-    
-    std::cout << "Image de base" << std::endl;
-    utils::print_image(std::cout, ima);
-
-    auto ima_ell = through(ima, utils::fun_sqr{});
-    std::cout << "Image ell" << std::endl;
-    utils::print_image(std::cout, ima_ell);
-
-    auto ima_bin = through(ima, utils::fun_equal{2});
-    std::cout << "Image bin" << std::endl;
-    utils::print_image(std::cout, ima_ell);
+    partial_image2d<bool_t> part_ima = partial_image2d<bool_t>(6,5,v);
+    auto im_t = through(part_ima, fun_zeros{});
 
 
+    std::cout<<"Image de base"<<std::endl;
+    utils::print_image(std::cout, part_ima);
+    auto d = compute_dmap(im_t);
+
+    auto data = d.getData();
+    partial_image2d<unsigned> part_d = partial_image2d<unsigned>(6,5,data);
+    part_d.setDomain(part_ima.domain());
+
+    std::cout<<"Image modifiee"<<std::endl;
+    utils::print_image(std::cout, part_d);
     return 0;
 }
